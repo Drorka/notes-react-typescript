bf09dddde797e5f24af279940e1b2860d6bb0036
Drorka~ <drorpa4@gmail.com>
2023-03-18 11:12:39 +0200

    support tags in notes

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,26 +1,32 @@
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap'
 import CreatableReactSelect from 'react-select/creatable'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FormEvent, useRef, useState } from 'react'
 import { v4 as uuidV4 } from 'uuid'
 import { NoteData, Tag } from '../App'
 
 type NoteFormProps = {
 	onSubmit: (data: NoteData) => void
+	onAddTag: (tag: Tag) => void
+	availableTags: Tag[]
 }
 
-export function NoteForm({ onSubmit }: NoteFormProps) {
+export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
 	const titleRef = useRef<HTMLInputElement>(null)
 	const markdownRef = useRef<HTMLTextAreaElement>(null)
 	const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+	const navigate = useNavigate()
 
 	function handleSubmit(e: FormEvent) {
 		e.preventDefault()
+
 		onSubmit({
 			title: titleRef.current!.value,
 			markdown: markdownRef.current!.value,
-			tags: [],
+			tags: selectedTags,
 		})
+
+		navigate('..')
 	}
 
 	return (
@@ -54,6 +60,9 @@ export function NoteForm({ onSubmit }: NoteFormProps) {
 										onAddTag(newTag)
 										setSelectedTags((prev) => [...prev, newTag])
 									}}
+									options={availableTags.map((tag) => {
+										return { label: tag.label, value: tag.id }
+									})}
 								/>
 							</Form.Group>
 						</Col>
